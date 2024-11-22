84365c3aed5353f756da1c71eb79e50bf0f27e92
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2024-11-22 22:48:26 +0530

    intro page updated

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,24 +1,28 @@
 import './IntroPage.css';
-import './Counter.css';
-import Counter from './Counter';
+// import './Counter.css';
+// import Counter from './Counter';
 
 function IntroPage({ isFirstVisit, getStartedFn }) {
   return (
-  <>
-  <Counter />
-    <div className="intro-container">
-      <h1 className="intro-head">
-        <span className="head-text">
-        <span>Supercharge your career and save time with our resume builder </span>
-       
-      </span>
-      </h1>
-      <p className="intro-desc">A free resume builder for developers & software engineers.</p>
-      <div className="start-btn-container">
-        <button type="button" onClick={getStartedFn} className="get-started-btn">CREATE RESUME</button>
+    <div className="wrapper">
+      <div className="intro-container">
+        <h1 className="intro-head">
+          <span className="head-text">
+            <span>Supercharge your career and save time with our resume builder</span>
+          </span>
+        </h1>
+        <p className="intro-desc">A free resume builder for developers & software engineers.</p>
+        <div className="start-btn-container">
+          <button
+            type="button"
+            onClick={getStartedFn}
+            className="get-started-btn"
+          >
+            CREATE RESUME
+          </button>
+        </div>
       </div>
     </div>
-    </>
   );
 }
 
