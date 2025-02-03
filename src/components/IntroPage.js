9e751fd2345d90dfb5ae06d27d231ed2b53a09ef
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2025-02-03 14:31:08 +0530

    counter added

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,8 +1,14 @@
-import './IntroPage.css';
-// import './Counter.css';
-// import Counter from './Counter';
+import { useEffect } from "react";
+import "./IntroPage.css";
 
-function IntroPage({ isFirstVisit, getStartedFn }) {
+function IntroPage({ getStartedFn }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div className="wrapper">
       <div className="intro-container">
@@ -13,14 +19,15 @@ function IntroPage({ isFirstVisit, getStartedFn }) {
         </h1>
         <p className="intro-desc">A free resume builder for developers & software engineers.</p>
         <div className="start-btn-container">
-          <button
-            type="button"
-            onClick={getStartedFn}
-            className="get-started-btn"
-          >
+          <button type="button" onClick={getStartedFn} className="get-started-btn">
             CREATE RESUME
           </button>
         </div>
+      </div>
+
+      {/* Elfsight Widget */}
+      <div className="elfsight-widget-container">
+        <div className="elfsight-app-0d381ebb-7870-47a1-83eb-028132dd944e" data-elfsight-app-lazy></div>
       </div>
     </div>
   );
